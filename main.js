72a9264bbab049464f127559745d18713a8802b6
Terry <hootonterry@gmail.com>
2024-10-01 15:19:29 +0100

    Corrected Card issues

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ async function start() {
 start()
 
 async function petsArea() {
-    const petsPromise = await fetch("https://teza1.github.io/pet-adoption/")
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
         const clone = template.content.cloneNode(true)
