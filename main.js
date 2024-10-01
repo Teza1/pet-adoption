225b5c871e93ff4232b96caab1cebdc5ee5d5220
Terry <hootonterry@gmail.com>
2024-10-01 14:19:09 +0100

    Updated Pet cards to live viewing

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
 
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LKN/82,93/forecast")
@@ -6,5 +8,27 @@ async function start() {
     const ourTemperature = weatherData.properties.periods[0].temperature
     document.querySelector("#temperature-output").textContent = ourTemperature
 }
-
 start()
+
+async function petsArea() {
+    const petsPromise = await fetch("https://teza1.github.io/pet-adoption/")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+
+        clone.querySelector(".pet-card").dataset.species = pet.species
+
+        clone.querySelector("h3").textContent = pet.name
+        clone.querySelector(".pet-description").textContent = pet.description
+        clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
+        if (!pet.photo) pet.photo = "images/fallback.jpg"
+
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+
+        wrapper.appendChild(clone)
+    })
+    document.querySelector(".list-of-pets").appendChild(wrapper)
+}
+petsArea()
